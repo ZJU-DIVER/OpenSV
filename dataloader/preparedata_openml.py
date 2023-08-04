b82bcf314e1186eeec7e810cd94087aea19b14ac
zx89898077 <3180103542@zju.edu.cn>
2023-08-04 15:08:29 +0800

    bug: fix data bug in data preprocessing

--- dataloader/preparedata_openml.py
+++ dataloader/preparedata_openml.py
@@ -5,34 +5,29 @@ Note！！！
 
 import numpy as np
 from sklearn.datasets import fetch_openml
+from sklearn.preprocessing import LabelEncoder
 
 base='../datasets'
 
-
-
-
-def get_processed_data(dataset, n_data ,  n_val , seed):
+def get_processed_data(dataset, id, n_data ,  n_val , seed):
     print('-------')
     print('Load Dataset {}'.format(dataset))
     np.random.seed(seed)
-    data,target=load_openml(dataset)
-
-    idxs=np.random.permutation(len(data))
-    data, target=data[idxs], target[idxs]
+    data, target = load_openml(id)
+    idxs = np.random.permutation(len(data))
+    data, target = data[idxs], target[idxs]
 
     x_train, y_train = data[:n_data], target[:n_data]
     x_val, y_val = data[n_data:n_data+n_val], target[n_data:n_data+n_val]
-    
+    # print(x_train, y_train)
     return x_train, y_train, x_val, y_val 
 
 
-
-
-def load_openml(dataset , is_classification=True, file_home = base):
+def load_openml(id, is_classification=True, file_home = base):
     """
         come from opendataval 0.0 hahahaha
     """
-    dataset = fetch_openml(dataset, as_frame=False,data_home=file_home)
+    dataset = fetch_openml(data_id=id, as_frame=False,data_home=file_home)
     category_list = list(dataset["categories"].keys())
     if len(category_list) > 0:
         category_indices = [dataset["feature_names"].index(x) for x in category_list]
@@ -46,6 +41,8 @@ def load_openml(dataset , is_classification=True, file_home = base):
     # label transformation
     if is_classification is True:
         list_of_classes, y = np.unique(y, return_inverse=True)
+        print(list_of_classes)
+        print(y)
     else:
         y = (y - np.mean(y)) / (np.std(y) + 1e-8)
 

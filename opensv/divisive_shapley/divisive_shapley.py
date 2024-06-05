f7d48d6b93ec802de0e10f2c2e578b76b2defb4e
haochengxia <xhc_1007@163.com>
2024-06-05 09:41:31 +0800

    feat: add exact SV

--- opensv/divisive_shapley/divisive_shapley.py
+++ opensv/divisive_shapley/divisive_shapley.py
@@ -1,6 +1,11 @@
-from typing import List, Callable, Tuple
+from typing import List, Callable, Tuple, Iterator
 import numpy as np
 from sklearn.cluster import KMeans
+from functools import partial
+from itertools import product, chain, combinations
+from multiprocessing import Pool
+from sklearn import neighbors
+from tqdm import tqdm, trange
 
 """
 References:
@@ -9,9 +14,46 @@ References:
 """
 
 
+def power_set(iterable) -> Iterator:
+    """Generate the power set of the all elements of an iterable obj.
+    """
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r)
+                               for r in range(1, len(s) + 1))
+
+
+def exact_shap(game, model):
+    """
+    Calculating the Shapley value of data points with exact method
+    (Shapley value definition)
+
+    :param model:    the selected model
+    :return: Shapley value array `sv`
+    :rtype: numpy.ndarray
+    """
+
+    n = game.num_players
+    ext_sv = np.zeros(n)
+    coef = np.zeros(n)
+    fact = np.math.factorial
+    coalition = np.arange(n)
+    for s in range(n):
+        coef[s] = fact(s) * fact(n - s - 1) / fact(n)
+    sets = list(power_set(coalition))
+    for idx in trange(len(sets)):
+        u = game.get_utility(list(sets[idx]))
+        for i in sets[idx]:
+            ext_sv[i] += coef[len(sets[idx]) - 1] * u
+        for i in set(coalition) - set(sets[idx]):
+            ext_sv[i] -= coef[len(sets[idx])] * u
+    return ext_sv
+
+
 def shapley_value_exact(S: np.ndarray, v: Callable[[np.ndarray], float]) -> np.ndarray:
     # Placeholder for exact Shapley value calculation
     # Implement the actual Shapley value computation for coalition S
+    # TODO: create a new game here and then invoke exact_shap
+
     return np.random.rand(len(S))
 
 
